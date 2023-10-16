8f566b5ed8f164a527867d4e87d3c0f48f23ee70
diegomp <diegom.mendoza@hotmail.com>
2023-10-15 19:27:26 -0500

    actualizacion

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from flask import Flask, request, render_template
+from pyexpat.errors import messages
+
+from flask import Flask, request, render_template, redirect
 import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder='templates')
-app.secret_key = "clave_larga"
+# app.secret_key = "clave_larga"
+
+# Configura la conexión a PostgreSQL
+host = "localhost"
+database_name = "ICO_Prices"
+user = "postgres"
+password = "97B8467b2b*"
+
+# Crea una cadena de conexión SQLAlchemy
+connection_string = f"postgresql+psycopg2://{user}:{password}@{host}/{database_name}"
+
+# Crea un motor de SQLAlchemy
+engine = create_engine(connection_string)
 
 
 @app.route("/")
@@ -12,16 +29,34 @@ def formulario():
 
 @app.route("/importar_excel", methods=["POST"])
 def importar_excel():
-    lista = []
-    archivo = request.files['archivosq']
-    df = pd.read_excel(archivo)
-    df['ValorTotal'] = df['PrecioUnidad'] * df['UnidadesEnExistencia']
-    df['PrecioVenta'] = (df['ValorTotal'] * 0.10 / 100) + df['ValorTotal']
-    df_resultado = (
-        df.loc[:, ['NombreProducto', 'Proveedor', 'PrecioUnidad', 'UnidadesEnExistencia', 'ValorTotal', 'PrecioVenta']])
+    mrp_data = request.files['mrpdata']
+    df_mrp_data = pd.read_excel(mrp_data)
+    df_mrp_data['Material'] = df_mrp_data['Material'].astype(int)
+    zptfr = request.files['zptfr']
+    df_zptfr = pd.read_excel(zptfr)
+    volume = request.files['volume']
+    df_volume = pd.read_excel(volume)
+    table_name1 = "MRP_DATA"
+    table_name2 = "ZPTFR_04"
+    table_name3 = "VOLUME_IN"
 
-    return render_template('tabla.html', tabla=[df_resultado.to_html(classes='data')], titles=df.columns.values)
+    df_mrp_data.to_sql(table_name1, con=engine, if_exists='replace', index=False)
+    df_zptfr.to_sql(table_name2, con=engine, if_exists='replace', index=False)
+    df_volume.to_sql(table_name3, con=engine, if_exists='replace', index=False)
 
+    df_informeconsolidado = df_volume[df_volume['Material'].isin(df_mrp_data['Material'])]
+    df_informeconsolidado['Status'] = df_mrp_data['Plant-sp.matl status']
+    df_informeconsolidado['Previous Price USD'] = 12.34
+    df_informeconsolidado['Actual Price USD'] = 14.06
+    df_informeconsolidado['% Variance ICO'] = round(
+        df_informeconsolidado['Actual Price USD'] / (df_informeconsolidado['Previous Price USD'] - 1), 2)
+    df_informeconsolidado['Variance ICO USD'] = round(
+        df_informeconsolidado['Actual Price USD'] - df_informeconsolidado['Previous Price USD'], 2)
+    df_informeconsolidado['Comments'] = "NINGUNO"
+    columnas_deseadas = ['Material', 'Material Description', 'Profit Center', 'UOM', 'LE2023', 'Status',
+                         'Previous Price USD', 'Actual Price USD', '% Variance ICO', 'Variance ICO USD', 'Comments']
+    df_informeconsolidado = df_informeconsolidado[columnas_deseadas]
+    return render_template('tabla.html', df_informeconsolidado=df_informeconsolidado)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
